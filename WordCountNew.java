9706ee9ccd3f9a883ad52cc48cacb10d7a01401e
Caitlin <Caitlin@Caitlings-MacBook-Air.local>
2018-04-16 16:32:10 +0200

    Edited file, parsing error.

--- WordCountNew.java
+++ WordCountNew.java
@@ -30,7 +30,7 @@ public class WordCountNew {
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
           while (values.hasNext()) {
-                sum += val.next().get();
+                sum += values.next().get();
             }
             output.collect(key, new IntWritable(sum));
         }
