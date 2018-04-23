befade99643e9c9e97156175e8cd3f792fb4541d
Caitlin <Caitlin@Caitlings-MacBook-Air.local>
2018-04-23 12:24:50 +0200

    Added counter.py

---
+++ counter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""counter.py"""
+
+import sys
+import json
+
+sys.stdout = open("count_tweets.txt", "w")
+count = 0
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # check if this next line contains a document and is not an empty line
+    if line.startswith("{"):
+        # use json.loads to convert tweet from json format to python
+        json_data = json.loads(line)
+        # check if retweeted_status is a key in json_data, if not it is not a retweet
+        if 'retweeted_status' not in json_data:
+            # if this is a original tweet, add to count
+            count += 1
+print (count)
+sys.stdout.close()
